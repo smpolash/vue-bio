506a1d9be7d6c162365e9465abe4b70aea1dda57
Shahin Mahmud <sm.polash@gmail.com>
2018-08-28 15:22:46 +0600

    options methods updated

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,47 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  beforeCreate: function () {
+    // Called after instance has beed initialized; before $data, $watch;
+    console.log('beforeCreate')
+  },
+  created: function () {
+    // Called just after setup: $data, computed props, methods, watch;
+    console.log('created')
+  },
+  beforeMount: function () {
+    // Called right before 'render' function to be called;
+    console.log('beforeMount')
+  },
+  mounted: function () {
+    // Called after vm.$el instance created;
+    console.log('mounted')
+  },
+  beforeUpdate: function () {
+    // Called when $data changes, before DOM is patched;
+    console.log('beforeUpdate')
+  },
+  updated: function () {
+    // Called after $data changes, Virtual DOM to be re-rendered & patched;
+    console.log('updated')
+  },
+  activated: function () {
+    console.log('activated')
+  },
+  deactivated: function () {
+    console.log('deactivated')
+  },
+  beforeDestroy: function () {
+    console.log('beforeDestroy')
+  },
+  destroyed: function () {
+    console.log('destroyed')
+  },
+  errorCaptured: function (err, vm, info) {
+    console.log(`${err.stack}\n\nfound in ${info} of component`)
+    return false
+  }
 }
 </script>
 
